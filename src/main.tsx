3961cf82a76685197952b83c8c00d82ffe709833
camward <camward@yandex.ru>
2025-04-15 23:02:13 +0400

    установка библиотек npm i @admiral-ds/react-ui npm i styled-components npm i react-router-dom@6.30.0 npm i -D prettier npm i react-hook-form @hookform/error-message @hookform/resolvers yup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import {
+  LIGHT_THEME,
+  FontsVTBGroup,
+  DropdownProvider,
+} from "@admiral-ds/react-ui";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={LIGHT_THEME}>
+      <DropdownProvider>
+        <FontsVTBGroup />
+        <App />
+      </DropdownProvider>
+    </ThemeProvider>
+  </React.StrictMode>,
+);
