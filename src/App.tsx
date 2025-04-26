ca62e968ae7c0a6a1e3a5a56396720047fd798ba
camward <camward@yandex.ru>
2025-04-26 09:26:34 +0400

    подключение skeleton npm install react-loading-skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,40 @@
-const App = () => <></>;
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Theme } from "@admiral-ds/react-ui";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const CssWrapper = styled.div`
+  color: ${(props) => (props.theme as Theme).color["Neutral/Neutral 90"]};
+`;
+
+const App = () => {
+  const [showSkeleton, setShowSkeleton] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setShowSkeleton(false);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, []);
+
+  return (
+    <CssWrapper>
+      {showSkeleton ? (
+        <Skeleton count={4} />
+      ) : (
+        <ul>
+          <li>WebSocket</li>
+          <li>Server-Sent Events</li>
+          <li>Long Polling</li>
+          <li>WebTransport</li>
+        </ul>
+      )}
+    </CssWrapper>
+  );
+};
 
 export default App;
